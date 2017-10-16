d5074b8a53dbf218c1aa3d8cf8ef7c6feef775aa
Andrej Babic <andrej.babic@psi.ch>
2017-10-16 12:03:21 +0200

    New library version
    - uid and gid are set to the same value

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mflow_nodes',
-    version="1.2.0",
+    version="1.2.1",
     description="mflow nodes is a library for building processing nodes on top of an mflow stream.",
     author='Paul Scherrer Institute',
     author_email='andrej.babic@psi.ch',
